dbff16ba47b2fe02bc2c2b7ec8c30d23db18fc48
Steele Walston <steele.walston@gmail.com>
2018-03-19 16:24:04 -0700

    made one GET

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT;
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+// const sa = require('superagent');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+const client = require('./db-client');
+
+app.get('/api/v1/spots', (request, response) => {
+    client.query(`
+        SELECT * FROM spots;
+    
+    `)
+        .then(results => response.send(results.rows))
+        .catch(error => {
+            console.error(error);
+            response.sendStatus(500);
+        });
+
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT);
+});
