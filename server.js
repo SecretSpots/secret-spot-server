8e94bb8c2255959ce266f419b0dbe89814cd77d8
Jen Lipton <jkl.lipton@gmail.com>
2018-03-20 09:12:45 -0700

    maps api key

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT;
+const MAPS_API_KEY = process.env.MAPS_API_KEY;
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
