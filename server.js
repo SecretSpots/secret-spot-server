778d90fdde9b7fc7859c51f67ee9474eb6b42408
Steele Walston <steele.walston@gmail.com>
2018-03-20 10:08:12 -0700

    app.post ready to test in postman

--- server.js
+++ server.js
@@ -32,6 +32,33 @@ app.get('/api/v1/spots', (request, response) => {
 
 });
 
+function insertSpot(spot) {
+    return client.query(`
+        INSERT INTO spots (
+            name, 
+            location,
+            note,
+            date,
+        )
+        VALUES ($2, $4, $5, $6)
+        RETURNING *;
+    `,
+    [spot.name, spot.location, spot.note, spot.date]
+    )
+        .then(result => result.rows[0]);
+}
+
+app.post('/api/spots/new', (request, response) => {
+    const body = request.body;
+
+    insertSpot(body)
+        .then(result => response.send(result))
+        .catch(error => {
+            console.error(error);
+            response.sendStatus(500);
+        });
+});
+
 app.listen(PORT, () => {
     console.log('Server running on port', PORT);
 });
