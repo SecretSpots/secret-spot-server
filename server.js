d0f53c8a2d26c3d28105fc37a815f4419fc0fbff
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-21 10:49:17 -0700

    hide delete button if unauthorized

--- server.js
+++ server.js
@@ -68,8 +68,10 @@ app.post('/api/v1/auth/signup', (request, response, next) => {
             );
         })
         .then(result => {
+            const user_id = result.rows[0].user_id;
             response.json({
-                token: makeToken(result.rows[0].user_id),
+                token: makeToken(user_id),
+                user_id: user_id,
                 username: result.rows[0].username
             });
         })
@@ -93,8 +95,10 @@ app.post('/api/v1/auth/signin', (request, response, next) => {
             if(result.rows.length === 0 || result.rows[0].password !== credentials.password) {
                 return next({ status: 401, message: 'invalid username or password' });
             }
+            const user_id = result.rows[0].user_id;
             response.json({
-                token: makeToken(result.rows[0].user_id),
+                token: makeToken(user_id),
+                user_id: user_id,
                 username: result.rows[0].username
             });
         });
