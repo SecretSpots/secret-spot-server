13a9cf79b22b0532d30590714637d3e3645556ed
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-21 00:08:11 -0700

    debug logout

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function validateUser(request, response, next) {
 }
 
 function makeToken(id) {
-    return { token: jwt.sign({ id: id }, TOKEN_KEY) };
+    return jwt.sign({ id: id }, TOKEN_KEY);
 }
 
 app.post('/api/v1/auth/signup', (request, response, next) => {
@@ -50,7 +50,7 @@ app.post('/api/v1/auth/signup', (request, response, next) => {
     client.query(`
         SELECT user_id
         FROM users
-        WHERE username=$1
+        WHERE username=$1;
    `,
     [credentials.username]
     )
@@ -68,8 +68,10 @@ app.post('/api/v1/auth/signup', (request, response, next) => {
             );
         })
         .then(result => {
-            const token = makeToken(result.rows[0].id);
-            response.send(token);
+            response.json({
+                token: makeToken(result.rows[0].id),
+                username: result.rows[0].username
+            });
         })
         .catch(next);
 });
@@ -81,9 +83,9 @@ app.post('/api/v1/auth/signin', (request, response, next) => {
     }
 
     client.query(`
-        SELECT user_id, password
+        SELECT user_id, password, username
         FROM users
-        WHERE username=$1
+        WHERE username=$1;
     `,
     [credentials.username]
     )
@@ -91,8 +93,10 @@ app.post('/api/v1/auth/signin', (request, response, next) => {
             if(result.rows.length === 0 || result.rows[0].password !== credentials.password) {
                 return next({ status: 401, message: 'invalid username or password' });
             }
-            const token = makeToken(result.rows[0].id);
-            response.send(token);
+            response.json({
+                token: makeToken(result.rows[0].id),
+                username: result.rows[0].username
+            });
         });
 });
 
